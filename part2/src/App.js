4f50de7a2b80af589220d49fb1bdf0749e988292
ta93n <nideratakumin@gmail.com>
2021-02-08 18:51:34 +0900

    part2(途中)

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)//全てのnoteを保持するstate
+  const [newNote, setNewNote] = useState(//フォーム入力内容を保持するstate
+    'a new note...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()//inputのsubmitクリックで発生する動作(ページリロード)を防止する
+    const noteObject = {
+      content: newNote,//newNote stateから値を受け取る
+      date: new Date().toISOString(),//
+      inportant: Math.random() < 0.5,//Math.random()は0~1間の値を返す 50%の確率で重要なメモと判断させる
+      id: notes.length + 1,//IDはnoteの総数に基づいて生成(noteが削除されない前提)
+    }
+
+    //concatメソッドによりnoteObjectが追加された新しいnotes配列を作成
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  //フォーム内容の変更をstateと同期させる
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -15,6 +40,13 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
