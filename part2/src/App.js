4985b890eedc5bd94c1f0e732f8e0fec37bfccd3
ta93n <nideratakumin@gmail.com>
2021-02-08 19:20:19 +0900

    part2(途中)

--- part2/src/App.js
+++ part2/src/App.js
@@ -3,9 +3,8 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)//全てのnoteを保持するstate
-  const [newNote, setNewNote] = useState(//フォーム入力内容を保持するstate
-    'a new note...'
-  )
+  const [newNote, setNewNote] = useState('')//フォーム入力内容を保持するstate
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()//inputのsubmitクリックで発生する動作(ページリロード)を防止する
@@ -27,16 +26,28 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  //条件演算子を用いて、showAllがtrueの場合は全てのnoteを、falseの場合は重要なnoteのみを返す
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+    //( filter()は与えられた関数によって実装されたテストに合格した全ての配列からなる新しい配列を生成 )
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        //showAllのtrue:falseを切り替える
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {/*
           mapメソッドによって生成された要素は
           それぞれ一意のキー値（keyと呼ばれる属性）を
           持っている必要がある。
         */}
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
