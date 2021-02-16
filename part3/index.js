95862cbcde6a14afa6640f13be7b63510c0db34a
ta93n <nideratakumin@gmail.com>
2021-02-16 21:00:08 +0900

    Express(単一のリソースを表示)

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,8 @@
-//httpモジュールでWebサーバー構築
-
-//Node.jsの組み込みWebサーバー構成モジュールを有効にする
 //Node.jsはモジュールをrequire()で呼び出して使う必要がある
-const http = require('http')
+//expressの導入
+const express = require('express')
+//app変数に配置されるExpressアプリケーションに対応するオブジェクトを作成する関数
+const app = express()
 
 let notes = [
   {
@@ -25,17 +25,38 @@ let notes = [
   }
 ]
 
-//createServer()メソッドを使ってWebサーバーを構築
-const app = http.createServer((request, response) => {
+/*
+アプリケーションに対してルート定義
+reqはapp.getの第一引数で指定されたパスに入ってきたHTTPリクエストを表すオブジェクト
+resは指定されたパスに入ってきたリクエストに対するHTTPレスポンスを構成するためのオブジェクト
+*/
+app.get('/', (req, res) => {
   //以下にサーバー処理を記述
-  //リクエストはステータスコード200で応答され、
-  //writeHead()でContent-Typeヘッダーをapplication/json(データがJSON形式であること)に設定
-  //end()の引数をブラウザに表示させるコンテンツに設定(JSON.stringify()メソッドによってjsonに変換)
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+  //sendメソッドでブラウザに文字列を送信
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  //jsonメソッドでパラメータ内のJavascriptオブジェクトに対応するJSON形式の文字列を送信
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  //Number()メソッドで、受け取ったパラメータの文字列IDを数値に変更(後のnote.idが数値なので)
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  //受け取ったIDに対応するnoteが見つからない場合はステータスコード404を返す
+  if (note) {
+    response.json(note)
+  } else {
+    //responseにデータが関連づけられていないため、status()に加えてend()する必要がある
+    response.status(404).end()
+  }
 })
 
 //listen()でポート番号を指定すれば、「localhost:3001」にブラウザからアクセスできる
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
